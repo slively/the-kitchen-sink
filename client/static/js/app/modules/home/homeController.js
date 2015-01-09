86b388f57da6ac45f1399da745b7e282e4a70ca0
Scott Lively <scott.lively@gmail.com>
2015-01-09 15:20:52 -0600

    renaming stuff

--- client/static/js/app/modules/home/homeController.js
+++ client/static/js/app/modules/home/homeController.js
@@ -1,6 +1,6 @@
 'use strict';
 
 module.exports = ['$scope', '$window', function($, $window) {
-    $.message = 'Welcome to the LAN party.';
+    $.message = 'Welcome to the kitchen sink.';
     $.config = $window.config;
 }];
--- client/tests/e2e/home.js
+++ client/tests/e2e/home.js
@@ -4,6 +4,6 @@ var driver = require('../helpers/e2e-driver'),
 describe('home page functional test', function() {
     it('should have a h3 title of \'Welcome to the LAN party.\'', function(done) {
         driver.get('http://localhost:3000/');
-        chai.expect('#home h3').dom.to.contain.text('Welcome to the LAN party.',done);
+        chai.expect('#home h3').dom.to.contain.text('Welcome to the kitchen sink.',done);
     }) ;
 });
