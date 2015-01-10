069ee3fbbf2e487f89fe68d3b71ac7d7458cfc07
Scott Lively <scott.lively@gmail.com>
2015-01-10 14:38:17 -0600

    add env to health check

--- server/routes/health-check.js
+++ server/routes/health-check.js
@@ -1,6 +1,7 @@
 module.exports = function healthCheck(server) {
+    var env = server.get('env');
     // health check endpoint for load balancers to ping
     server.get('/health-check/',function(req,res){
-        res.status(200).end('OK');
+        res.status(200).end('OK ' + env);
     });
 };
